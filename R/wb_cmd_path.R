5744b44ccf1bf3bd7d6140792d6f7f9ad83952dd
Damon Pham <damondpham@gmail.com>
2020-07-24 09:09:19 -0700

    removed files that shouldn't have been added to the merge! patch for wb_command directory in other OS systems. explicitly added rh_linux.

--- R/wb_cmd_path.R
+++ R/wb_cmd_path.R
@@ -1,19 +1,20 @@
 wb_path_request <- function() {
-  return(paste(
+  paste(
     "\n******************************************************************************************",
     "ciftiTools requires the path to the Connectome Workbench folder, or directly to the", 
     "`wb_command(.exe)`. Please execute `ciftiTools.setOption('wb_path', 'path/to/workbench')`.", 
     "Or, provide the path to each function with the `wb_path` argument.",
     "******************************************************************************************\n",
     sep='\n'
-  ))
+  )
 }
 
-#' Retrieves the Connectome Workbench path from the ciftiTools_wb_path option or the provided argument.
-#' This path can be the Connectome Workbench folder or the executable directly.
+#' Retrieves the Connectome Workbench path from the \code{ciftiTools_wb_path} 
+#'  option or the provided argument. This path can be the Connectome Workbench
+#'  folder or the `wb_command` executable.
 #' 
 #' @inheritParams wb_path_Param
-#' @param verbose Default FALSE.
+#' @param verbose Default: \code{FALSE}.
 #'
 #' @export
 #'
@@ -44,16 +45,33 @@ get_wb_cmd_path <- function(wb_path=NULL, verbose=FALSE) {
 
   infer_wb_cmd_path <- function(wb_path) {
     files_in_path <- list.files(wb_path)
+
+    # Check known OS first.
     if ("bin_linux64" %in% files_in_path) {
       wb_cmd_path <- file.path(wb_path, "bin_linux64", "wb_command")
+    } else if ("bin_rh_linux64" %in% files_in_path) {
+      wb_cmd_path <- file.path(wb_path, "bin_rh_linux64", "wb_command")
     } else if ("bin_macosx64" %in% files_in_path) {
       wb_cmd_path <- file.path(wb_path, "bin_macosx64", "wb_command")
     } else if ("bin_windows64" %in% files_in_path) {
       wb_cmd_path <- file.path(wb_path, "bin_windows64", "wb_command.exe")
     } else {
+      
+      # Fail value: NULL
       wb_cmd_path <- NULL
+
+      # If no recognized OS found, try regex.
+      is_bin <- grepl("bin", files_in_path, fixed=TRUE)
+      if (sum(is_bin) == 1) {
+        bin <- files_in_path[is_bin]
+        bin_files <- list.files(file.path(wb_path, bin))
+        is_cmd <- grepl("wb_command", bin_files)
+
+        if (sum(is_cmd) == 1) {
+          wb_cmd_path <- file.path(wb_path, bin, bin_files[is_cmd])
+        }
+      }
     }
-    return(wb_cmd_path)
   }
 
   if (!dir.exists(wb_path)) {
@@ -64,7 +82,7 @@ get_wb_cmd_path <- function(wb_path=NULL, verbose=FALSE) {
       # Maybe the zip superfolder was used.
       wb_cmd_path <- infer_wb_cmd_path(file.path(wb_path, "workbench")) 
       if (is.null(wb_cmd_path)) {
-        stop(paste0("The Workbench folder ", wb_path, " exists but does not contain the wb_cmd in a bin_[OS]64 folder."))
+        stop(paste0("The Workbench folder ", wb_path, " exists but does not contain the wb_cmd in a `bin_[OS]64` folder. Tried to look for a file with `wb_command` in its name located in a folder with `bin` in its name, but this also failed (no such unique file)."))
       }
     }
     if (!file.exists(wb_cmd_path)) {
@@ -72,5 +90,5 @@ get_wb_cmd_path <- function(wb_path=NULL, verbose=FALSE) {
     }
   }
 
-  return(wb_cmd_path)
+  wb_cmd_path
 }
