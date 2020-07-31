dd815a4d2f39739925807db4e8eb7472aedcecd2
Damon Pham <damondpham@gmail.com>
2020-07-31 03:00:36 -0700

    draft new read_cifti (is a wrapper)

--- R/read_cifti.R
+++ R/read_cifti.R
@@ -14,13 +14,14 @@
 #' @inheritParams cifti_fname_Param
 #' @param format One of \code{"regular"}, \code{"flat"}, or \code{"minimal"}.
 #' 
-#'  If \code{"regular"}, the result will be a "cifti" object. See 
-#'  \code{\link{is.cifti}} with \code{flat==FALSE}.
+#'  If \code{"regular"}, the result will be a \code{"cifti"} object. See 
+#'  \code{\link{is.cifti}} with \code{flat==FALSE}. 
 #' 
-#'  If \code{"flat"}, the result will be a "cifti_flat" object. See 
+#'  If \code{"flat"}, the result will be a \code{"cifti_flat"} object. See 
 #'  \code{\link{is.cifti}} with \code{flat==TRUE}. If 
 #'  \code{brainstructures=="all"}, the "DAT" component will be identical to the
-#'  data matrix created by \code{-cifti-convert -to-gifti-ext}.
+#'  data matrix created by \code{-cifti-convert -to-gifti-ext}, except the 
+#'  order of the subcortical voxels will be spatial instead of alphabetical.
 #' 
 #'  If \code{"minimal"}, the result will be a T x B matrix (T measurements, B 
 #'  non-empty brainordinates). The \code{method} and \code{brainstructures} 
@@ -36,17 +37,24 @@
 #' 
 #' @inheritParams surfL_fname_Param
 #' @inheritParams surfR_fname_Param
-#' @inheritParams brainstructures_Param_LR
+#' @inheritParams brainstructures_Param_LR 
 #' @param method Either \code{"convert"} (default) or \code{"separate"}. Only 
 #'  applies if \code{format=="regular"} or \code{format=="flat"}.
 #' 
 #'  If \code{"convert"}, the CIFTI will be read in using the 
 #'  \code{-cifti-convert -to-gifti-ext} Workbench Command. If \code{"separate"},
 #'  it will be read in using the \code{-cifti-separate} Workbench Command. 
-#'  The former is much faster, but the latter is necessary for resampling, 
+#'  The former is faster and memory-efficient if the subcortical data are
+#'  requested, but the latter is necessary for resampling, 
 #'  obtaining the full NIFTI volume (without cropping empty edge slices), or 
 #'  working with the ROIs. Aside from those limitations the results are 
 #'  identical, so the more efficient method \code{"convert"} is the default.
+#' @param resamp_res Resolution to resample the cortical data and surface to.
+#'  Resampling is only possible if \code{method=="separate"}. Spheres in the
+#'  original resolution are required (\code{sphereL_fname} and 
+#'  \code{sphereR_fname}).
+#' @inheritParams sphereL_fname_Param
+#' @inheritParams sphereR_fname_Param
 #' @inheritParams wb_path_Param
 #' @param ... Additional arguments to \code{read_cifti_minimal} if 
 #'  \code{method=="convert"}, or \code{read_cifti_separate} if
@@ -73,12 +81,16 @@ read_cifti <- function(
   surfL_fname=NULL, surfR_fname=NULL,
   brainstructures=c("left","right"), 
   method=c("convert", "separate"),
-  wb_path=NULL, ...){
+  resamp_res=NULL, sphereL_fname=NULL, sphereR_fname=NULL,
+  wb_path=NULL, 
+  ...){
 
-  stop("Doesn't work.")
+  # ----------------------------------------------------------------------------
+  # Check arguments. -----------------------------------------------------------
+  # ----------------------------------------------------------------------------
 
   format <- match.arg(format, c("regular", "flat", "minimal"))
-  if (format=="minimal") { method="convert" }
+  if (format == "minimal") { method = "convert" }
   method <- match.arg(method, c("convert", "separate"))
 
   brainstructures <- match_input(
@@ -93,100 +105,56 @@ read_cifti <- function(
   do_sub <- "subcortical" %in% brainstructures
 
   dots <- list(...)
-  if ("resamp_res" %in% dots && method == "convert") {
+  if (!is.null(resamp_res) && method == "convert") {
     stop( "To resample, set `method= \"separate\"`." )
   }
 
   # ----------------------------------------------------------------------------
-  # If `format=="minimal"`, just get the data and finish. ----------------------
-  # Otherwise, create the template. --------------------------------------------
+  # Read. ----------------------------------------------------------------------
   # ----------------------------------------------------------------------------
 
   if (format=="minimal") {
-    return( read_cifti_minimal(cifti_fname, wb_path=wb_path) )
+    return(read_cifti_minimal(
+      cifti_fname, wb_path=wb_path
+    ))
+
   } else if (format=="regular") {
-    stop("Doesn't work.")
+    return(read_cifti_separate(
+      cifti_fname,
+      surfL_fname=surfL_fname, surfR_fname=surfR_fname,
+      brainstructures=brainstructures,
+      resamp_res=resamp_res, 
+      sphereL_fname=sphereL_fname, sphereR_fname=sphereR_fname,
+      wb_path=wb_path,
+      ...
+    ))
+
   } else if (format=="flat") {
     return(read_cifti_flat(
       cifti_fname, 
       surfL_fname=surfL_fname, surfR_fname=surfR_fname, 
       brainstructures=brainstructures, 
-      wb_path=wb_path
+      wb_path=wb_path,
+      ...
     ))
   } else { stop() }
-
-
-  #   if (format == "regular") {
-  #     flat_idx <- cifti$LABELS$SUBSTRUCTURE != "MEDIAL_WALL"
-  #     left_idx <- flat_idx & cifti$LABELS$BRAINSTRUCTURE == "CORTEX_LEFT"
-  #     right_idx <- flat_idx & cifti$LABELS$BRAINSTRUCTURE == "CORTEX_RIGHT"
-  #     sub_idx <- flat_idx & cifti$LABELS$BRAINSTRUCTURE == "SUBCORT"
-  #     if (do_left) {
-  #       cifti$CORTEX_LEFT <- cifti_dat[left_idx,]
-  #     }
-  #     if (do_right) {
-  #       cifti$CORTEX_RIGHT <- cifti_dat[right_idx,]
-  #     }
-  #     if (do_subcort) {
-  #       cifti$SUBCORT <- cifti_dat[sub_idx,]
-  #     }
-
-  #   } else if (format == "flat") {
-  #     cifti$DAT <- cifti_dat[cifti$LABELS$BRAINSTRUCTURE %in% brainstructures,]
-      
-  #   } else { stop() }
-
-  #   if (!is.null(surfL)) { cifti$SURF_LEFT <- make_cifti_surface(surfL) }
-  #   if (!is.null(surfR)) { cifti$SURF_RIGHT <- make_cifti_surface(surfR) }
-
-  # } else if (method == "separate") {
-  #   cifti_components <- read_cifti_separate(
-  #     cifti_fname,
-  #     surfL_fname=NULL, surfR_fname=NULL,
-  #     map=cifti_map,
-  #     brainstructures=brainstructures,
-  #     wb_path=wb_path, ...
-  #   )
-
-  #   if (format == "regular") {
-  #     cifti$CORTEX_LEFT <- cifti_components$CORTEX_LEFT
-  #     cifti$CORTEX_RIGHT <- cifti_components$CORTEX_RIGHT
-  #     cifti$SUBCORT <- cifti_components$SUBCORT
-
-  #   } else if (format=="flat") {
-  #     cifti$DAT <- rbind(
-  #       cifti_components$CORTEX_LEFT,
-  #       cifti_components$CORTEX_RIGHT,
-  #       cifti_components$SUBCORT
-  #     )[cifti$LABELS$SUBSTRUCTURE != "MEDIAL_WALL",]
-
-  #   } else { stop() }
-
-  #   stopifnot(identical(cifti$LABELS$SUBCORT, cifti_components$SUBCORT_LABELS))
-  #   stopifnot(identical(cifti$META$SUBCORT_MASK, cifti_components$SUBCORT_MASK))
-  #   cifti$META$SUBCORT_MASK_PADDING <- cifti_components$SUBCORT_MASK_PADDING
-  #   cifti$SURF_LEFT <- cifti_components$SURF_LEFT
-  #   cifti$SURF_RIGHT <- cifti_components$SURF_RIGHT
-
-  # } else { stop() }
-
-  # cifti
 }
 
-# #' @rdname read_cifti
-# #' @export
-# readCIfTI <- readcii <- function(
-#   cifti_fname, format=c("regular", "flat", "minimal"),
-#   surfL_fname=NULL, surfR_fname=NULL,
-#   brainstructures=c("left","right"), 
-#   method=c("convert", "separate"),
-#   wb_path=NULL, ...){
+#' @rdname read_cifti
+#' @export
+readCIfTI <- readcii <- function(
+  cifti_fname, format=c("regular", "flat", "minimal"),
+  surfL_fname=NULL, surfR_fname=NULL,
+  brainstructures=c("left","right"), 
+  method=c("convert", "separate"),
+  resamp_res=NULL, sphereL_fname=NULL, sphereR_fname=NULL,
+  wb_path=NULL, ...){
 
-#   read_cifti(
-#     cifti_fname, format,
-#     surfL_fname, surfR_fname,
-#     brainstructures, 
-#     method,
-#     wb_path, ...
-#   )
-# }
+  read_cifti(
+    cifti_fname, format,
+    surfL_fname, surfR_fname,
+    brainstructures, 
+    method,
+    wb_path, ...
+  )
+}
