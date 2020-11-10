62a18f929184ce063917d923106844db7b717e7b
Damon Pham <damondpham@gmail.com>
2020-11-10 05:43:13 -0800

    Patch resampling test. Reduce max code line length in vignette.

--- tests/testthat/test-resampling.R
+++ tests/testthat/test-resampling.R
@@ -23,35 +23,37 @@ test_that("Resampling CIFTI and GIFTI files is working", {
       resamp_res=5000
     )
 
-    # read_cifti with resampling, with surfaces
-    cii <- readcii(
-      cii_fname, brainstructures=brainstructures,
-      resamp_res=10000, surfL=fnames$surf["left"], surfR=fnames$surf["right"]
-    )
+    if (cii_fname == "dtseries") {
+      # read_cifti with resampling, with surfaces
+      cii <- readcii(
+        cii_fname, brainstructures=brainstructures,
+        resamp_res=10000, surfL=fnames$surf["left"], surfR=fnames$surf["right"]
+      )
 
-    # resample_surf
-    surf <- resample_surf(cii$surf$cortex_left, hemisphere="left", resamp_res=3000)
+      # resample_surf
+      surf <- resample_surf(cii$surf$cortex_left, hemisphere="left", resamp_res=3000)
 
-    # resample_cifti
-    cii2_fnames <- resample_cifti(
-      cii_fname, basename(cii_fname), resamp_res=4001,
-      surfL_original_fname = fnames$surf["left"],
-      surfR_original_fname = fnames$surf["right"],
-      surfL_target_fname = basename(fnames$surf["left"]),
-      surfR_target_fname = basename(fnames$surf["right"]),
-      write_dir = tdir
-    )
-    cii2 <- readcii(
-      cii2_fnames["cifti"],
-      surfL_fname=cii2_fnames["surfL"], surfR_fname=cii2_fnames["surfR"]
-    )
+      # resample_cifti
+      cii2_fnames <- resample_cifti(
+        cii_fname, basename(cii_fname), resamp_res=4001,
+        surfL_original_fname = fnames$surf["left"],
+        surfR_original_fname = fnames$surf["right"],
+        surfL_target_fname = basename(fnames$surf["left"]),
+        surfR_target_fname = basename(fnames$surf["right"]),
+        write_dir = tdir
+      )
+      cii2 <- readcii(
+        cii2_fnames["cifti"],
+        surfL_fname=cii2_fnames["surfL"], surfR_fname=cii2_fnames["surfR"]
+      )
 
-    # resample_cifti_from_template
-    cii3 <- readcii(resample_cifti_from_template(
-      original=cii_fname, template=cii2_fnames["cifti"],
-      target=file.path(tdir, paste0("v2_", basename(cii_fname)))
-    ))
+      # resample_cifti_from_template
+      cii3 <- readcii(resample_cifti_from_template(
+        original=cii_fname, template=cii2_fnames["cifti"],
+        target=file.path(tdir, paste0("v2_", basename(cii_fname)))
+      ))
 
-    try(testthat::expect_equal(cii2$data, cii3$data))
+      try(testthat::expect_equal(cii2$data, cii3$data))
+    }
   }
 })
