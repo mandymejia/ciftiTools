e54b2c749c56d8907f0712a58a19484e77f3b1e8
Damon Pham <damondpham@gmail.com>
2021-06-22 19:05:05 -0700

    reorder function listings

--- R/ciftiTools-package.R
+++ R/ciftiTools-package.R
@@ -4,15 +4,15 @@
 #'  \code{library(help="ciftiTools")} for a complete list of user functions.)
 #' 
 #' \itemize{
-#'  \item{\code{make_xifti}:}{ Create one from data.}
-#'  \item{\code{newdata_xifti}:}{ Replace the data.}
 #'  \item{\code{read_xifti}:}{ Read in CIFTI data as a \code{"xifti"}.}
-#'  \item{\code{resample_xifti}:}{ Resample the data.}
-#'  \item{\code{smooth_xifti}:}{ Smooth the data.}
-#'  \item{\code{transform_xifti}:}{ Apply univariate or bivariate functions over the data.}
+#'  \item{\code{write_xifti}:}{ Write a \code{"xifti"} to a CIFTI file.}
 #'  \item{\code{view_xifti_surface}:}{ Plot the cortical data.}
 #'  \item{\code{view_xifti_volume}:}{ Plot the subcortical data.}
-#'  \item{\code{write_xifti}:}{ Write a \code{"xifti"} to a CIFTI file.}
+#'  \item{\code{resample_xifti}:}{ Resample the data.}
+#'  \item{\code{smooth_xifti}:}{ Smooth the data.}
+#'  \item{\code{make_xifti}:}{ Create one from data.}
+#'  \item{\code{newdata_xifti}:}{ Replace the data.}
+#'  \item{\code{transform_xifti}:}{ Apply univariate or bivariate functions over the data.}
 #' }
 #'
 #' @docType package
