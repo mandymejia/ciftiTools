45f50c6abdf197456fc189a33773981c1f425215
Damon Pham <damondpham@gmail.com>
2022-01-25 04:36:18 -0500

    patch to `scale_xifti`

--- R/scale_xifti.R
+++ R/scale_xifti.R
@@ -8,17 +8,18 @@
 #' @return The input \code{"xifti"} with scaled columns.
 #' 
 #' @keywords export
-scale_xifti <- function(x, center=TRUE, scale=TRUE){
-  stopifnot(is.xifti(x))
+scale_xifti <- function(xifti, center=TRUE, scale=TRUE){
+  stopifnot(is.xifti(xifti))
 
-  if (!is.null(x$meta$cifti$intent)) {
-    if (x$meta$cifti$intent == 3007) {
+  if (!is.null(xifti$meta$cifti$intent)) {
+    if (xifti$meta$cifti$intent == 3007) {
       ciftiTools_warn("Converting to `dscalar` before scaling.\n")
-      x <- convert_xifti(x, "dscalar")
+      xifti <- convert_xifti(xifti, "dscalar")
     }
   }
 
-  x <- newdata_xifti(x,
-    scale(as.matrix(x), center=center, scale=scale)
+  xifti <- newdata_xifti(
+    xifti,
+    scale(as.matrix(xifti), center=center, scale=scale)
   )
 }
