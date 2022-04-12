892ae426cf0615d9712f25e475b78fd6fc12f277
Damon Pham <damondpham@gmail.com>
2022-04-12 16:13:35 -0400

    Draft new welcome message.

--- R/onAttach.R
+++ R/onAttach.R
@@ -10,11 +10,27 @@
 .onAttach <- function(...) {
   if (interactive()) {
     if (is.null(getOption("ciftiTools_wb_path"))) {
-      packageStartupMessage(wb_path_request())
+      packageStartupMessage(welcome_msg())
     }
   }
   ciftiTools.setOption("EPS", 1e-8)
   ciftiTools.setOption("suppress_msgs", TRUE)
 
   invisible(NULL)
+}
+
+#' Welcome message
+#' 
+#' Print the welcome message.
+#' 
+#' @keywords internal
+#' @return The message (length 1 character vector)
+welcome_msg <- function() {
+  paste0(
+    "\n*****************************************************************\n",
+    "*     Welcome to ciftiTools! Thanks for using our package.      *\n",
+    "*              Please cite our paper in your work:              *\n", 
+    "*           doi.org/10.1016/j.neuroimage.2022.118877            *",
+    wb_path_request()
+  )
 }
