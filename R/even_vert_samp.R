bb3f53a56ff3ccdd841cea5f806f00f201e98203
Damon Pham <damondpham@gmail.com>
2023-03-22 15:49:34 +0700

    Add `even_vert_samp`

---
+++ R/even_vert_samp.R
@@ -0,0 +1,34 @@
+#' Evenly sample vertices of mesh
+#'
+#' Get a subset of the mesh vertices that are spatially evenly-sampled, by
+#'  resampling the mesh and choosing the original vertices closest (Euclidian
+#'  distance) to the new vertices.
+#'
+#' @param surf A \code{"surf"} object
+#' @param n_vert The desired number of vertices in the evenly-spaced sample.
+#'  Note that the actual size of the subset will likely be close to but not
+#'  exactly \code{n_vert} because it depends on the size of the resampled
+#'  surface.
+#'
+#' @return An integer vector giving the indices of the vertices in the subset.
+#'
+#' @importFrom fMRItools is_posNum
+#'
+even_vert_samp <- function(surf, n_vert) {
+  stopifnot(is.surf(surf))
+  stopifnot(fMRItools::is_posNum(n_vert))
+
+  vert_og <- t(surf$vertices) # 3 x original_res
+  vert_rs <- t(resample_surf(surf, n_vert)$vertices) # 3 x n_vert
+
+  # The size of the resampled surface is not exactly `n_vert`.
+  n_vert <- ncol(vert_rs)
+
+  vmask <- rep(NA, n_vert)
+  for (ii in seq(n_vert)) {
+    vdist <- colSums((vert_og - vert_rs[,ii])^2)
+    vmask[ii] <- which.min(vdist)
+  }
+
+  vmask
+}
